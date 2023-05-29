2c0bc1240405f6f3ba333d9e0b35e8e594b0c0c2
Adrian <hadrian3689@gmail.com>
2023-05-29 13:54:33 -0400

    Added day4

---
+++ 2015/day4/day4a.py
@@ -0,0 +1,14 @@
+import hashlib
+
+def count(md5_hash):
+    for i in range(0,9999999):
+        new_value = md5_hash + str(i)
+        hashing = hashlib.md5(new_value.encode())
+        new_hash = hashing.hexdigest()
+        if new_hash[:5] == "00000":
+            print(i)
+            exit()
+
+if __name__=="__main__":
+    md5_hash = "bgvyzdsv"
+    count(md5_hash)
